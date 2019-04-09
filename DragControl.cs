55c401b3f33e25909dbd628e2300b6e63200ba68
MuratDumlu <40153345+MuratDumlu@users.noreply.github.com>
2019-04-09 22:42:47 +0300

    Add files via upload

---
+++ DragControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragControl
+{
+    class DragControl : Component
+    {
+        private Control handleControl;
+        public Control SelectControl
+        {
+            get
+            {
+                return this.handleControl;
+            }
+            set
+            {
+                this.handleControl = value;
+                this.handleControl.MouseDown += new MouseEventHandler(this.DragForm_MouseDown);
+            }
+        }
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+        public extern static void ReleaseCapture();
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        public extern static void SendMessage(IntPtr a, int msg, int wParam, int lParam);
+
+        private void DragForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            bool flag = e.Button == MouseButtons.Left;
+            if (flag)
+            {
+                ReleaseCapture();
+                SendMessage(this.SelectControl.FindForm().Handle, 0x112, 0xf012, 0);
+            }
+        }
+    }
+}
